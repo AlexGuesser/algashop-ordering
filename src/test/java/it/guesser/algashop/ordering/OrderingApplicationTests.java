42173d790f26cedac8172fdde0efe7b4320ac856
Axonify-Alex-Guesser <aguesser@axonify.com>
2026-01-22 10:02:45 -0300

    Creating project

---
+++ src/test/java/it/guesser/algashop/ordering/OrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package it.guesser.algashop.ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
